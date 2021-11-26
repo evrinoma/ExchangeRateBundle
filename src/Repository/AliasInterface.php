a118ca5bcf7838ba318b8574efc3d4c94c10e0ad
Nikolay Nikolaev <evrinoma@gmail.com>
2021-11-27 00:31:45 +0300

    init Alias

---
+++ src/Repository/AliasInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Evrinoma\ExchangeRateBundle\Repository;
+
+interface AliasInterface
+{
+//region SECTION: Fields
+    public const RATE  = 'rate';
+    public const TYPE  = 'type';
+//endregion Fields
+}
