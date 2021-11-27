9b1f9d40c96a21c5d1f31bebc2fd3de854308aa8
Nikolay Nikolaev <evrinoma@gmail.com>
2021-11-27 11:32:01 +0300

    fix Model

--- src/Model/ModelInterface.php
+++ src/Model/ModelInterface.php
@@ -5,8 +5,8 @@ namespace Evrinoma\ExchangeRateBundle\Model;
 interface ModelInterface
 {
 //region SECTION: Fields
-    public const ID       = 'id';
-    public const ACTIVE   = 'active';
-    public const IDENTITY = 'identity';
+    public const ID      = 'id';
+    public const CREATED = 'timestamp';
+    public const VALUE   = 'value';
 //endregion Fields
 }
