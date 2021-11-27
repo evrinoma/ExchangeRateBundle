2f6f37c2daeab52e96c64e3847188d72c5e90b34
Nikolay Nikolaev <evrinoma@gmail.com>
2021-11-27 11:31:51 +0300

    fix Dto

--- src/Dto/RateApiDto.php
+++ src/Dto/RateApiDto.php
@@ -2,26 +2,175 @@
 
 namespace Evrinoma\ExchangeRateBundle\Dto;
 
+use Evrinoma\DtoBundle\Annotation\Dto;
 use Evrinoma\DtoBundle\Dto\AbstractDto;
 use Evrinoma\DtoBundle\Dto\DtoInterface;
 use Evrinoma\DtoCommon\ValueObject\IdTrait;
+use Evrinoma\ExchangeRateBundle\Model\ModelInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 class RateApiDto extends AbstractDto implements RateApiDtoInterface
 {
     use IdTrait;
 
+//region SECTION: Fields
+    /**
+     * @Dto(class="Evrinoma\ExchangeRateBundle\Dto\TypeApiDto", generator="genRequestBaseApiDto")
+     * @var TypeApiDto|null
+     */
+    private ?TypeApiDto $baseApiDto = null;
+
+    /**
+     * @Dto(class="Evrinoma\ExchangeRateBundle\Dto\TypeApiDto", generator="genRequestTypeApiDto")
+     * @var TypeApiDto|null
+     */
+    private ?TypeApiDto $typeApiDto = null;
+
+    private float $value = 0;
+
+    private ?\DateTimeImmutable $created = null;
+//endregion Fields
+
+//region SECTION: Protected
+    /**
+     * @param float $value
+     */
+    protected function setValue(float $value): void
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @param \DateTimeImmutable|null $created
+     */
+    protected function setCreated(\DateTimeImmutable $created): void
+    {
+        $this->created = $created;
+    }
+//endregion Protected
+
+//region SECTION: Private
+    /**
+     * @param int|null $id
+     */
+    private function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+//endregion Private
 
 //region SECTION: Dto
+    /**
+     * @return \Generator
+     */
+    public function genRequestTypeApiDto(?Request $request): ?\Generator
+    {
+        if ($request) {
+            $type = $request->get('type');
+            if ($type) {
+                $newRequest                    = $this->getCloneRequest();
+                $type[DtoInterface::DTO_CLASS] = TypeApiDto::class;
+                $newRequest->request->add($type);
+
+                yield $newRequest;
+            }
+        }
+    }
+
+    /**
+     * @return \Generator
+     */
+    public function genRequestBaseApiDto(?Request $request): ?\Generator
+    {
+        if ($request) {
+            $type = $request->get('base');
+            if ($type) {
+                $newRequest                    = $this->getCloneRequest();
+                $type[DtoInterface::DTO_CLASS] = TypeApiDto::class;
+                $newRequest->request->add($type);
+
+                yield $newRequest;
+            }
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasBaseApiDto(): bool
+    {
+        return $this->baseApiDto !== null;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasTypApiDto(): bool
+    {
+        return $this->typeApiDto !== null;
+    }
+
+    /**
+     * @param TypeApiDto|null $baseApiDto
+     */
+    public function setBaseApiDto(?TypeApiDto $baseApiDto): void
+    {
+        $this->baseApiDto = $baseApiDto;
+    }
+
+    /**
+     * @param TypeApiDto|null $typeApiDto
+     */
+    public function setTypeApiDto(?TypeApiDto $typeApiDto): void
+    {
+        $this->typeApiDto = $typeApiDto;
+    }
+
     public function toDto(Request $request): DtoInterface
     {
         $class = $request->get(DtoInterface::DTO_CLASS);
 
         if ($class === $this->getClass()) {
+            $id      = $request->get(ModelInterface::ID);
+            $created = $request->get(ModelInterface::CREATED);
+            $value   = $request->get(ModelInterface::VALUE);
 
+            if ($created) {
+                $this->setCreated(new \DateTimeImmutable($created));
+            }
+
+            if ($value) {
+                $this->setValue((float)\str_replace(',', '.', $value));
+            }
+
+            if ($id) {
+                $this->setId($id);
+            }
         }
 
         return $this;
     }
+
+    public function getBaseApiDto(): TypeApiDtoInterface
+    {
+        return $this->baseApiDto;
+    }
+
+    public function getTypeApiDto(): TypeApiDtoInterface
+    {
+        return $this->typeApiDto;
+    }
 //endregion SECTION: Dto
+
+//region SECTION: Getters/Setters
+    public function getValue(): float
+    {
+        return $this->value;
+    }
+
+    public function getCreated(): \DateTimeImmutable
+    {
+        return $this->created;
+    }
+//endregion Getters/Setters
 }
--- src/Dto/RateApiDtoInterface.php
+++ src/Dto/RateApiDtoInterface.php
@@ -7,5 +7,15 @@ use Evrinoma\DtoCommon\ValueObject\IdInterface;
 
 interface RateApiDtoInterface extends DtoInterface, IdInterface
 {
+//region SECTION: Dto
+    public function getBaseApiDto(): TypeApiDtoInterface;
 
+    public function getTypeApiDto(): TypeApiDtoInterface;
+//endregion SECTION: Dto
+
+//region SECTION: Getters/Setters
+    public function getValue(): float;
+
+    public function getCreated(): \DateTimeImmutable;
+//endregion Getters/Setters
 }
